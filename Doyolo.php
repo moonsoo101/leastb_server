5db2185a94b763649bb73bdab81585889fd0102b
moonsoo101 <moonsoo101@gmail.com>
2017-06-16 18:25:17 +0000

    isAccident update 부분 삭제

--- Doyolo.php
+++ Doyolo.php
@@ -1,7 +1,7 @@
 <?php
 $filename = $_POST['filename'];
-// $filename = "auto";
 
+// $filename = "auto";
 $last_line = system('./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights data/'.$filename.'.jpg -out '.$filename, $retval);
 // $last_line = system('./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights data/auto.jpg -out auto', $retval);
  // $last_line = system('./darknet detect cfg/yolo.cfg yolo.weights data/image1.jpg -out image1', $retval);
@@ -9,20 +9,20 @@ $last_line = system('./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg 
 
 // echo $last_line;
 if(strpos($last_line, "car") !== false)
-{
-  //  system('mv '.$filename.'.jpg results/' .$filename.'.jpg', $retval);
   push("차량에 주의하세요.", $filename);
-}
+
 else if(strpos($last_line, "motorbike") !== false)
 push("오토바이에 주의하세요.", $filename);
+
 else if(strpos($last_line, "bicycle") !== false)
 push("자전거에 주의하세요.", $filename);
+
 else if(strpos($last_line, "traffic light") !== false)
 push("행단보도에 주의하세요.", $filename);
+
   function push($message, $filename)
   {
-    updateIsAccident($filename);
-    $params = array ('message' => $message,'imgurl' => 'http://ec2-13-124-33-214.ap-northeast-2.compute.amazonaws.com/darknet/'.$filename.'.jpg');
+    $params = array ('message' => $message,'imgurl' => $filename);
     $query = http_build_query ($params);
     // Create Http context details
     $contextData = array (
@@ -41,22 +41,35 @@ push("행단보도에 주의하세요.", $filename);
                       $context);
                       echo $result;
   }
-  function updateIsAccident($filename)
-  {
-    $con = mysqli_connect("ec2-13-124-108-18.ap-northeast-2.compute.amazonaws.com", "root", "leastb", "fcm");
-  	mysqli_set_charset($con,"utf8");
-  	if (mysqli_connect_errno($con))
-  	{
-  		 echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  	}
-  	$query = "update image set isaccident = 1 where imgname = '$filename';";
-  	$res = mysqli_query($con,$query);
-    $query = "select watchdayID from image where imgname = '$filename';";
-  	$res = mysqli_query($con,$query);
-    $row = mysqli_fetch_array($res);
-  	$WDindex = $row['watchdayID'];
-    $query = "update watchday set accident = 1 where WDindex = $WDindex;";
-  	$res = mysqli_query($con,$query);
-  	mysqli_close($con);
-  }
+  // function updateIsAccident($isAccident)
+  // {
+  //   $con = mysqli_connect("ec2-13-124-108-18.ap-northeast-2.compute.amazonaws.com", "root", "leastb", "fcm");
+  // 	// mysqli_set_charset($con,"utf8");
+  // 	// if (mysqli_connect_errno($con))
+  // 	// {
+  // 	// 	 echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  // 	// }
+  // 	// $query = "UPDATE image SET isaccident = 1 WHERE imgname = '$_POST['filename']';";
+  // 	// $res = mysqli_query($con,$query);
+  //   // $query = "select watchdayID from image where imgname = '$_POST['filename']';";
+  // 	// $res = mysqli_query($con,$query);
+  //   // $row = mysqli_fetch_array($res);
+  // 	// $WDindex = $row['watchdayID'];
+  //   // echo $WDindex;
+  //   // $query = "UPDATE watchday SET accident = 1 WHERE WDindex = $WDindex;";
+  // 	// $res = mysqli_query($con,$query);
+  // 	// mysqli_close($con);
+  // 	mysqli_set_charset($con,"utf8");
+  // 	if (mysqli_connect_errno($con))
+  // 	{
+  // 		 echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  // 	}
+  // 	$query = "select WDindex from watchday where id ='$id' and year = $year and month = $month and day = $day;";
+  // 	$res = mysqli_query($con,$query);
+  // 	$row = mysqli_fetch_array($res);
+  // 	$WDindex = $row['WDindex'];
+  // 	$query = "insert into image (watchdayID, imgname, latitude, longitude, isaccident) values ($WDindex, '$imgName', '$latitude', '$longitude', $isAccident);";
+  // 	$res = mysqli_query($con,$query);
+  // 	mysqli_close($con);
+  // }
 ?>
